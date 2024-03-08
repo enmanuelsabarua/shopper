caeca27df88b1b774e5d61844be6bcc58e72890d
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-08 19:15:19 -0400

    feat: add item component

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "../Components/Hero/Hero"
+import { Popular } from "../Components/Popular/Popular"
 
 export const Shop = () => {
   return (
     <div>
         <Hero />
+        <Popular />
     </div>
   )
 }
