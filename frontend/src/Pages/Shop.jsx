44250ae6542c0cd81f64a1884d10ead703a9bf39
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-09 10:34:56 -0400

    feat: add offers banner

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import { Hero } from "../Components/Hero/Hero"
+import { Offers } from "../Components/Offers/Offers"
 import { Popular } from "../Components/Popular/Popular"
 
 export const Shop = () => {
@@ -6,6 +7,7 @@ export const Shop = () => {
     <div>
         <Hero />
         <Popular />
+        <Offers />
     </div>
   )
 }
