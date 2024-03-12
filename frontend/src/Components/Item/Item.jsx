728468ea0814bfd5919b5be3b075b575460b59ee
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-12 12:04:59 -0400

    feat: add related products component

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,7 +4,7 @@ import './Item.css';
 export const Item = ({ id, image, name, new_price, old_price }) => {
     return (
         <div className="item">
-            <Link to={`/product/${id}`}><img src={image} alt="Item" /></Link>
+            <Link to={`/product/${id}`}><img onClick={window.scrollTo(0, 0)} src={image} alt="Item" /></Link>
             <p>{name}</p>
             <div className="item-prices">
                 <div className="item-price-new">
