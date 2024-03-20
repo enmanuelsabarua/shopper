0f9d95d839018bab52864e489d9d34dccd91921d
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-20 10:33:01 -0400

    feat: make cart responsive

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -36,7 +36,7 @@ export const CartItems = () => {
             })}
             <div className="cartitems-down">
                 <div className="cartitems-total">
-                    <h1>Cart Totals</h1>
+                    <h1>Cart totals</h1>
                     <div>
                         <div className="cartitems-total-item">
                             <p>Subtotal</p>
