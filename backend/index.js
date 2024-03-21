39b3c7b8285f785190691bfcd5468de7906ddd9c
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-21 12:09:05 -0400

    feat: create upload endpoint for images

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,27 @@ app.use(cors());
 mongoose.connect(process.env.MONGODBURL);
 
 // API Creation
-
 app.get('/', (req, res) => {
     res.send('Express app is running');
+});
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage });
+
+// Creating Upload Endpoint for images
+app.use('/images', express.static('upload/images'));
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${PORT}/images/${req.file.filename}`
+    });
 });
 
 app.listen(PORT, error => {
