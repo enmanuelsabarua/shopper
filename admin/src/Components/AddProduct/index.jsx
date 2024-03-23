f9c2c3833b224cd90bb798295e89c6ac44ca6b58
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-23 11:20:51 -0400

    feat: connect add product to the backend

--- admin/src/Components/AddProduct/index.jsx
+++ admin/src/Components/AddProduct/index.jsx
@@ -4,31 +4,75 @@ import { useState } from 'react';
 
 const AddProduct = () => {
     const [image, setImage] = useState(false);
+    const [productDetails, setProductDetails] = useState({
+        name: '',
+        image: '',
+        category: 'women',
+        new_price: '',
+        old_price: '',
+    });
 
     const imageHandler = e => {
         console.log(e.target.files);
         setImage(e.target.files[0]);
     }
 
+    const changeHandler = e => {
+        setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+    }
+
+    const addProduct = async () => {
+        console.log(productDetails);
+        let responseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product', image);
+
+        await fetch('http://localhost:4000/upload', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+        },
+        body: formData,
+        })
+        .then(res => res.json())
+        .then(data => responseData = data);
+
+        if (responseData.success) {
+            product.image = responseData.image_url;
+            await fetch('http://localhost:4000/addproduct', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product),
+            })
+            .then(res => res.json())
+            .then(data => data.success ? alert('Product Added') : alert('Failed'));
+        }
+    }
+
     return (
         <div className='add-product'>
             <div className="addproduct-itemfield">
                 <p>Product title</p>
-                <input type="text" name='name' placeholder='Type here' />
+                <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
             </div>
             <div className="addproduct-price">
                 <div className="addproduct-itemfield">
                     <p>Price</p>
-                    <input type="text" name='old_price' placeholder='Type here' />
+                    <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='Type here' />
                 </div>
                 <div className="addproduct-itemfield">
                     <p>Offer Price</p>
-                    <input type="text" name='new_price' placeholder='Type here' />
+                    <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='Type here' />
                 </div>
             </div>
             <div className="addproduct-itemfield">
                 <p>Product Category</p>
-                <select name="category" className='addproduct-selector'>
+                <select value={productDetails.category} onChange={changeHandler} name="category" className='addproduct-selector'>
                     <option value="women">Women</option>
                     <option value="men">Men</option>
                     <option value="kid">Kid</option>
@@ -40,7 +84,7 @@ const AddProduct = () => {
                 </label>
                 <input onChange={imageHandler} type="file" name='image' id='file-input' hidden />
             </div>
-            <button className='addproduct-btn'>ADD</button>
+            <button onClick={() => addProduct()} className='addproduct-btn'>ADD</button>
         </div>
     )
 }
