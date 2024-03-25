5178f297e7d4fd9bf5c8e28e3b23e99c93d29a12
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-25 10:02:44 -0400

    feat: add the signup logic

--- backend/index.js
+++ backend/index.js
@@ -134,6 +134,64 @@ app.get('/allproducts', async (req, res) => {
     }
 });
 
+// Schema creating for User model
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// Creating Endpoint for registering the user
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+    let check = await Users.findOne({ email });
+
+    if (check) {
+        return res.status(400).json({
+            success: false,
+            error: 'existing user found with same email address'
+        });
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name,
+        email,
+        password,
+        cartData: cart,
+    });
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({
+        success: true,
+        token
+    });
+});
+
 app.listen(PORT, error => {
     if (!error) {
         console.log(`Server running on port ${PORT}`);
