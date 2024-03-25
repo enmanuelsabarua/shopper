bb31c65f5c16e72ad02014bac020aed71437d1dd
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-25 11:33:49 -0400

    feat: fetch products from the API

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,25 @@
-import { createContext, useState } from 'react';
-import all_product from '../Components/Assets/all_product';
+import { createContext, useEffect, useState } from 'react';
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let index = 0; index < all_product.length + 1; index++) {
+    for (let index = 0; index < 300 + 1; index++) {
         cart[index] = 0;
     }
     return cart;
 }
 
 const ShopContextProvider = ({ children }) => {
+
+    const [all_product, setAll_product] = useState([]);
     const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    useEffect(() => {
+        fetch('http://localhost:4000/allproducts')
+        .then(res => res.json())
+        .then(data => setAll_product(data))
+    }, []);
 
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
